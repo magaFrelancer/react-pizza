82149bf0b587bf7b019ce2b27cabc50db2527034
magaFrelancer <ban0le4@gmail.com>
2020-12-05 21:17:32 +0300

    router

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+
 import './scss/app.scss';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+   <Router>
+       <App/>
+   </Router>
   </React.StrictMode>,
   document.getElementById('root')
 );
